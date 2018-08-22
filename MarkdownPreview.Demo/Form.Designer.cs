85bf1c7c0dfc73d865cfd8259d292d8fb989e3e0
Robert Sirre <robert@cs.nl>
2018-08-22 23:22:15 +0200

    Added winforms demo app

---
+++ MarkdownPreview.Demo/Form.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace MarkdownPreview.Demo
+{
+	partial class Form
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.webBrowser = new System.Windows.Forms.WebBrowser();
+			this.splitContainer = new System.Windows.Forms.SplitContainer();
+			this.treeView = new System.Windows.Forms.TreeView();
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer)).BeginInit();
+			this.splitContainer.Panel1.SuspendLayout();
+			this.splitContainer.Panel2.SuspendLayout();
+			this.splitContainer.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// webBrowser
+			// 
+			this.webBrowser.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.webBrowser.Location = new System.Drawing.Point(0, 0);
+			this.webBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+			this.webBrowser.Name = "webBrowser";
+			this.webBrowser.Size = new System.Drawing.Size(716, 599);
+			this.webBrowser.TabIndex = 1;
+			// 
+			// splitContainer
+			// 
+			this.splitContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.splitContainer.Location = new System.Drawing.Point(0, 0);
+			this.splitContainer.Name = "splitContainer";
+			// 
+			// splitContainer.Panel1
+			// 
+			this.splitContainer.Panel1.Controls.Add(this.treeView);
+			// 
+			// splitContainer.Panel2
+			// 
+			this.splitContainer.Panel2.Controls.Add(this.webBrowser);
+			this.splitContainer.Size = new System.Drawing.Size(1079, 599);
+			this.splitContainer.SplitterDistance = 359;
+			this.splitContainer.TabIndex = 2;
+			// 
+			// treeView
+			// 
+			this.treeView.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.treeView.Location = new System.Drawing.Point(0, 0);
+			this.treeView.Name = "treeView";
+			this.treeView.Size = new System.Drawing.Size(359, 599);
+			this.treeView.TabIndex = 0;
+			// 
+			// Form
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1079, 599);
+			this.Controls.Add(this.splitContainer);
+			this.Name = "Form";
+			this.Text = "Markdown Preview test";
+			this.splitContainer.Panel1.ResumeLayout(false);
+			this.splitContainer.Panel2.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer)).EndInit();
+			this.splitContainer.ResumeLayout(false);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+		private System.Windows.Forms.WebBrowser webBrowser;
+		private System.Windows.Forms.SplitContainer splitContainer;
+		private System.Windows.Forms.TreeView treeView;
+	}
+}
+
---
+++ MarkdownPreview.Demo/Form.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security;
+using System.Security.Permissions;
+using System.Windows.Forms;
+using static MarkdownPreview.Core.Engine;
+
+namespace MarkdownPreview.Demo
+{
+	public partial class Form : System.Windows.Forms.Form
+	{
+
+		public Form()
+		{
+			InitializeComponent();
+
+			treeView.NodeMouseClick += treeView_NodeMouseClick;
+			treeView.BeforeExpand += treeView_NodeExpand;
+			treeView.AfterSelect += treeView_NodeSelected;
+
+			PopulateTreeView();
+		}
+
+		private void treeView_NodeSelected(object sender, TreeViewEventArgs e)
+		{
+			TreeNode newSelected = e.Node;
+			ShowContent(newSelected);
+		}
+
+		private void treeView_NodeExpand(object sender, TreeViewCancelEventArgs e)
+		{
+			foreach (TreeNode node in e.Node.Nodes)
+				if ((node.Tag is DirectoryInfo dir))
+					GetDirectories(dir, node);
+		}
+
+		private void PopulateTreeView()
+		{
+
+			foreach (var drive in DriveInfo.GetDrives())
+			{
+				var info = drive.RootDirectory;
+				if (info.Exists)
+				{
+					var rootNode = new TreeNode(info.Name)
+					{
+						Tag = info
+					};
+					GetDirectories(info, rootNode);
+					treeView.Nodes.Add(rootNode);
+
+					rootNode.Expand();
+				}
+			}
+		}
+
+		//private static bool HasReadAccess(string path)
+		//{
+
+		//	FileIOPermission writePermission = new FileIOPermission(FileIOPermissionAccess.PathDiscovery, path);
+		//	return SecurityManager.IsGranted(writePermission);
+		//}
+
+		public static bool HasReadAccess(string path)
+		{
+			var permissionSet = new PermissionSet(PermissionState.None);
+			var writePermission = new FileIOPermission(FileIOPermissionAccess.PathDiscovery, path);
+			permissionSet.AddPermission(writePermission);
+
+			return permissionSet.IsSubsetOf(AppDomain.CurrentDomain.PermissionSet);
+		}
+
+		private static void GetDirectories(DirectoryInfo dir, TreeNode nodeToAddTo)
+		{
+
+			if (!HasReadAccess(dir.FullName))
+			{
+				nodeToAddTo.Nodes.Add(
+					new TreeNode("No access", 0, 0)
+					{
+						BackColor = Color.Gray
+					});
+				return;
+			}
+
+			try
+			{
+				foreach (DirectoryInfo subDir in dir.GetDirectories())
+				{
+					var aNode = new TreeNode(subDir.Name, 0, 0)
+					{
+						Tag = subDir,
+						ImageKey = "folder"
+					};
+
+					nodeToAddTo.Nodes.Add(aNode);
+				}
+
+
+				var files = dir.GetFiles();
+				if (files.Any())
+					GetFiles(files, nodeToAddTo);
+			}
+			catch (UnauthorizedAccessException ex)
+			{
+				nodeToAddTo.Nodes.Add(
+					new TreeNode("Access denied", 0, 0)
+					{
+						BackColor = Color.Gray
+					});
+			}
+			catch (Exception ex)
+			{
+				nodeToAddTo.Nodes.Add(
+					new TreeNode(ex.Message, 0, 0)
+					{
+						BackColor = Color.Red
+					});
+
+				Trace.WriteLine($"{ex}");
+			}
+		}
+		private static void GetFiles(FileInfo[] files, TreeNode nodeToAddTo)
+		{
+			foreach (var file in files)
+				nodeToAddTo.Nodes.Add(
+					new TreeNode(file.Name, 0, 0)
+					{
+						Tag = file,
+						ImageKey = "file",
+						ForeColor = IsMarkDown(file)
+							? Color.Blue
+							: Color.Black
+					});
+
+		}
+
+		void treeView_NodeMouseClick(object sender, TreeNodeMouseClickEventArgs e)
+		{
+			TreeNode newSelected = e.Node;
+			ShowContent(newSelected);
+
+		}
+
+		private void ShowContent(TreeNode newSelected)
+		{
+			string content;
+			try
+			{
+				content = (newSelected.Tag is FileInfo file)
+												? IsMarkDown(file)
+													? file.Length > (10 * 1024 * 1024) // Let's not load 10MB MarkDown files
+														? $"File exceeds maximum file length ({file.Length:n0} bytes)"
+														: File.ReadAllText(file.FullName)
+													: $"`{file.Name}` is no MarkDown file"
+												: "No Markdown file selected";
+			}
+			catch (Exception ex)
+			{
+				content = $@"An error ocurred while reading file:
+```
+{ex}
+```";
+			}
+
+			var html = MarkDownToHtml(content);
+
+			webBrowser.DocumentText = html;
+		}
+
+		private static bool IsMarkDown(FileInfo nodeFileInfo)
+		{
+			return (new[] { ".md", ".markdown" }).Contains(nodeFileInfo.Extension.ToLower());
+		}
+	}
+
+}
