e443a6b8d30c70b7284b59e65021732471cdad34
Robert Sirre <robert@cs.nl>
2020-01-14 00:49:39 +0100

    Upgraded FxCop analysis

--- MarkdownPreview.Demo/Program.cs
+++ MarkdownPreview.Demo/Program.cs
@@ -1,22 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace MarkdownPreview.Demo
-{
-	static class Program
-	{
+namespace MarkdownPreview.Demo {
+	static class Program {
 		/// <summary>
 		/// The main entry point for the application.
 		/// </summary>
 		[STAThread]
-		static void Main()
-		{
+		static void Main() {
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
-			Application.Run(new Form());
+			using (var frm = new Form()) {
+				Application.Run(frm);
+			}
 		}
 	}
 }
